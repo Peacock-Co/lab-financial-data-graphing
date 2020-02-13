0cd7132aa5ffd535e34d3deb8cf9bddb5223ca57
Peacock-Co <pavaoleonardo@hotmail.com>
2020-02-13 13:32:06 +0100

    Added chart.js but can not link it to my index file

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+const apiUrl = `https://api.coindesk.com/v1/bpi/currentprice.json`;
+
+axios
+  .get(apiUrl)
+  .then(res => console.log("#########################################", res))
+  .catch(err => console.log("Error while getting the data: ", err));
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
 module.exports = router;
