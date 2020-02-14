e59f170d8a4018ac50fc07bb8f0d8a743ce331b2
Peacock-Co <pavaoleonardo@hotmail.com>
2020-02-14 13:06:59 +0100

    Added axios to the index.html

--- starter-code/public/javascripts/financial-data.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = `https://api.coindesk.com/v1/bpi/currentprice.json`;
-
-axios
-  .get(apiUrl)
-  .then(res => {
-    printTheChart(res.data)
-    console.log(res)
-  })
-  .catch(err => console.log("Error while getting the data: ", err));
